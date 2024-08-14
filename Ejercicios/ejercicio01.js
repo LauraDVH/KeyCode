efd1fe2ceca0c3c351c9b6e4a61938f2f2e0f755
LauraDVH <Daniela.vargas@tbtbglobal.com>
2024-08-13 20:53:14 -0500

    multiplication tables

---
+++ Ejercicios/ejercicio01.js
@@ -0,0 +1,10 @@
+// tablas de multiplicar 1 al 10 
+let num1 = 1
+let num2 = 1    
+for (let j = 1; j <=10; j++){
+    console.log(`Tabla del ${j}`)
+    for (let i = 1; i <= 10; i++){
+        console.log(`${j} x ${i} = ${j*i}`)
+    }
+    console.log('----------------------')
+}
