ff97305b1c98f8ffe32b9c52934bfd1dc0dffc63
LauraDVH <Daniela.vargas@tbtbglobal.com>
2024-08-14 19:13:03 -0500

    prueba

--- Ejercicios/ejercicio01.js
+++ Ejercicios/ejercicio01.js
@@ -1,6 +1,6 @@
+// Ejercicio-01  13/08/2024
 // tablas de multiplicar 1 al 10 
-let num1 = 1
-let num2 = 1    
+   
 for (let j = 1; j <=10; j++){
     console.log(`Tabla del ${j}`)
     for (let i = 1; i <= 10; i++){
@@ -8,3 +8,4 @@ for (let j = 1; j <=10; j++){
     }
     console.log('----------------------')
 }
+//hola este es un nuevo commit
