affaccbdf8e6d5a1425f70d3f5a56c7679b34d7a
LauraDVH <Daniela.vargas@tbtbglobal.com>
2024-09-11 20:53:43 -0500

    repo en grupo

---
+++ Ejercicios/react-router/src/pages/index.js
@@ -0,0 +1,6 @@
+export { Home } from './Home'
+export { Acerca } from './Acerca'
+export { Productos } from './Productos'
+export { Servicios } from './Servicios'
+export { Error404 } from './Error404'
+export { ProductosDetalle } from './ProductosDetalle'
